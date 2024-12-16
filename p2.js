b789054d4b2cd677c22a771aaf513f4146ce0f4b
Michał Urbanek <urbanekmike@gmail.com>
2024-12-16 17:25:45 +0100

    generator update

--- p2.js
+++ p2.js
@@ -1,4 +1,4 @@
-function cleanResults(data, locator = 'xzsf02u">') {
+function cleanResults(data, niedzielneGranie, locator = 'xzsf02u">') {
     //let locator='xzsf02u">'
     endPos = 0
     let endLocator = '</span'
@@ -39,17 +39,121 @@ function cleanResults(data, locator = 'xzsf02u">') {
 
     let found = [data1, data4, data6, data8, data10, data12, data14, data16, data18, data20, data22, data24, data26, data28, data30, data32
     ]
-    found = found.filter((el)=>!el.includes("Znajomi")&&!el.includes("Inni"))
-    let knownGoalkeepers=["Andrzej Kruczyński", "Szymon Śleziona", "Kamil Kawa", "Rafał Chrzanowski"]
+    found = found.filter((el) => !el.includes("Znajomi") && !el.includes("Inni"))
+    let knownGoalkeepers = ["Andrzej Kruczyński", "Szymon Śleziona", "Kamil Kawa", "Rafał Chrzanowski"]
     for (let index = 0; index < found.length; index++) {
-        if(knownGoalkeepers.includes(found[index])){
-            found[index] = found[index]+ "(BR)"
+        if (found[index].includes('<')) {
+            found[index] = found[index].substring(0, found[index].length - 1)
+        }
+        if (knownGoalkeepers.includes(found[index])) {
+            found[index] = found[index] + "(BR)"
         }
     }
-    let outPlayers = ""
-    for (let el of found) {
-        outPlayers = outPlayers + '<br></br>' + el
+    let stableRanking = {
+        "Bartek Pryszcz": 6,
+        "Dawid Will": 8,
+        "Daniel Toporczyk": 7,
+        "Bogumił Gr": 6.5,
+        "Illia Leo Ti Lish": 1.2,
+        "Paweł Maciejewski": 8.6,
+        "Paweł Macieje": 8.6,
+        "Roma Samkovskiy": 4.3,
+        "Yura Savchuk": 5,
+        "Marek Ziober": 4.8,
+        "Kamil Kawa(BR)": 6,
+        "Michał Siewniak": 5.8,
+        "Rafał Chrzanowski(BR)": 3.5,
+        "Adam Wisniewski": 7.8,
+        "Andrzej Kruczyński(BR)": 3.7,
+        "Aleksander Osmałek": 4.2,
+        "Andrzej Doruchowski": 3.8,
+        "Adam Syrek": 8.2,
+        "Oliwer Sulima": 3.9,
+        "Michal Ce": 4,
+        "Tobiasz Fuczek": 7.9,
+        "Adam Piątek": 6.8,
+        "Mateusz Szyba": 9,
+        "Bartek Zda": 8.4,
+        "Bartek Zdanowski": 8.4,
+        "Szymon Śleziona(BR)": 3.8,
+        "Marcin Szkup": 1,
+        "Daniel Tochwin": 8.2,
+        "Michał Urbanek": 4.1,
+        "Mateusz Ziober": 0.9,
+        "Marcin Ziober": 1
     }
-    document.getElementById('result').innerHTML = outPlayers
-    console.log("input" + outPlayers)
+    if (niedzielneGranie) {
+        stableRanking["Adam Piątek"] = stableRanking["Adam Piątek"] - 1.4
+        stableRanking["Paweł Maciejewski"] = stableRanking["Paweł Maciejewski"] - 1.4
+        stableRanking["Paweł Macieje"] = stableRanking["Paweł Maciejewski"] - 1.4
+    }
+    let playersWithRating = new Map()
+    found.forEach(element => {
+        playersWithRating.set(element, stableRanking[element])
+    });
+    playersWithRating.forEach((value, key) => {
+        if (value === undefined) {
+            playersWithRating.set(key, 4.9);
+        }
+    });
+    const sortedByRating = new Map(
+        [...playersWithRating].sort((a, b) => b[1] - a[1]) // Compare values
+    );
+    console.log(sortedByRating);
+    const keysIterator = sortedByRating.keys();
+    let team1 = [keysIterator.next().value]
+    let team2 = [keysIterator.next().value, keysIterator.next().value]
+    let playerToAdd = keysIterator.next().value;
+    let playerToAdd2 = keysIterator.next().value;
+    addIfGkToOtherTeam(playerToAdd, team1, team2);
+    addIfGkToOtherTeam(playerToAdd2, team1, team2);
+    playerToAdd = keysIterator.next().value;
+    playerToAdd2 = keysIterator.next().value;
+    addIfGkToOtherTeam(playerToAdd, team2, team1);
+    addIfGkToOtherTeam(playerToAdd2, team2, team1);
+    playerToAdd = keysIterator.next().value;
+    playerToAdd2 = keysIterator.next().value;
+    addIfGkToOtherTeam(playerToAdd, team1, team2);
+    addIfGkToOtherTeam(playerToAdd2, team1, team2);
+    playerToAdd = keysIterator.next().value;
+    playerToAdd2 = keysIterator.next().value;
+    addIfGkToOtherTeam(playerToAdd, team2, team1);
+    addIfGkToOtherTeam(playerToAdd2, team2, team1);
+    playerToAdd = keysIterator.next().value;
+    playerToAdd2 = keysIterator.next().value;
+    addIfGkToOtherTeam(playerToAdd, team1, team2);
+    addIfGkToOtherTeam(playerToAdd2, team1, team2);
+    playerToAdd = keysIterator.next().value;
+    addIfGkToOtherTeam(playerToAdd, team2, team1);
+
+    let finalAssign = "Team 1    Team 2"
+    console.log("Team 1    Team 2")
+    for (let index = 0; index < 7; index++) {
+        finalAssign = finalAssign + '<br></br>'
+        console.log('\n')
+        finalAssign = finalAssign + team1[index] + ' / ' + team2[index]
+
+        console.log(team1[index])
+        console.log(' / ')
+        console.log(team2[index])
+    }
+    document.getElementById('result').innerHTML = finalAssign
+
+    function addIfGkToOtherTeam(player, team1, team2) {
+        const containsSubstring = team1.some(element => element.includes('(BR)'));
+        if (containsSubstring) {
+            if (player.includes(('(BR)'))) {
+                console.log("GK FOUND ")
+                if(team2.length = 7){
+                    team1.push(team2.pop())
+                }
+                team2.push(player)
+            } else {
+                team1.push(player)
+            }
+        } else {
+            console.log("GK Not found")
+            team1.push(player)
+        }
+    }
 }
